a6f0c79f8d9d3ac38d3a2ff302d73f14dcbc5982
lazojeisson <jeissonlazo@gmail.com>
2024-03-28 18:40:31 -0500

    uupdate contourns

--- contours.py
+++ contours.py
@@ -1,15 +1,27 @@
 import cv2 as cv
+import numpy as np
 
 img = cv.imread("Photos/cats.jpg")
-
 cv.imshow("Cats", img)
+
+blank = np.zeros(img.shape, dtype="uint8")
+cv.imshow("Blank", blank)
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow("gray", gray)
 
-canny = cv.Canny(img, 125, 175)
+blur = cv.GaussianBlur(gray, (5, 5), cv.BORDER_DEFAULT)
+cv.imshow("blur", blur)
+
+canny = cv.Canny(blur, 125, 175)
 cv.imshow("canny", canny)
 
-contours, hierarchies
+# ret, thresh = cv.threshold(gray, 125, 255, cv.THRESH_BINARY)
+# cv.imshow("thresh", thresh)
 
+contours, hierarchies = cv.findContours(canny, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
+print(len(contours), "contours found!")
+
+cv.drawContours(blank, contours, -1, (0, 0, 255), 1)
+cv.imshow("Contours Drawn", blank)
 cv.waitKey(0)
